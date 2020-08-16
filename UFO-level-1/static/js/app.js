e8d84f624b37352a53ab5010e6e28840bb50588c
Michael Gleixner <gleixner.mike@gmail.com>
2020-08-16 15:47:21 -0400

    Final Commit of homework assignment

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -16,36 +16,36 @@ formGet.on("submit", DateEntered);
 
 // Complete the event handler function for the form
 function DateEntered() {
-    console.log("Something was clicked");
-    console.log(d3.event.target);
+  console.log("Something was clicked");
+  console.log(d3.event.target);
 
-    // Prevent the page from refreshing so details remain on the screen.
-    d3.event.preventDefault();
-    
-    // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#datetime");
+  // Prevent the page from refreshing so details remain on the screen.
+  d3.event.preventDefault();
 
-    // Get the value property of the input element
-    var inputValue = inputElement.property("value");
-  
-    console.log("Input Value Next");
-    console.log(inputValue);
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
 
-    var filteredData = tableData.filter(date_Data => date_Data.datetime === inputValue);
-  
-    console.log("Filtered Data Next");
-    console.log(filteredData);
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
 
-    //Clean-up previous data in the html table if it is there.
-    tbody.html("")
+  console.log("Input Value Next");
+  console.log(inputValue);
 
-    //load the HTML table to display data.
-    filteredData.forEach((UFOsighting) => {
-        var row = tbody.append("tr");
-        Object.entries(UFOsighting).forEach(([key, value]) => {
-          var cell = row.append("td");
-          cell.text(value);
-        });
+  var filteredData = tableData.filter(date_Data => date_Data.datetime === inputValue);
+
+  console.log("Filtered Data Next");
+  console.log(filteredData);
+
+  //Clean-up previous data in the html table if it is there.
+  tbody.html("");
+
+  //load the HTML table to display data.
+  filteredData.forEach((UFOsighting) => {
+    var row = tbody.append("tr");
+    Object.entries(UFOsighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
     });
-      
-};
+  });
+
+}
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -17,82 +17,82 @@ button.on("click", DateEntered);
 
 // Complete the event handler function for the form
 function DateEntered() {
-    console.log("Something was clicked");
-    console.log(d3.event.target);
-    // Prevent the page from refreshing so details remain on the screen.
-    d3.event.preventDefault();
-    
-    // Select the input element and get the raw HTML node
-    var inputDateTimeElement = d3.select("#datetime");
-    var inputCityElement = d3.select("#city");
-    var inputStateElement = d3.select("#state");
-    var inputCountryElement = d3.select("#country");
-    var inputShapeElement = d3.select("#shape");
+  console.log("Something was clicked");
+  console.log(d3.event.target);
+  // Prevent the page from refreshing so details remain on the screen.
+  d3.event.preventDefault();
 
-    // Get the value property of the input elements
-    var inputDateTime = inputDateTimeElement.property("value");
-    var inputCity = inputCityElement.property("value");
-    var inputState = inputStateElement.property("value");
-    var inputCountry = inputCountryElement.property("value");
-    var inputShape = inputShapeElement.property("value");
-  
-    console.log(`Input Date--> ${inputDateTime}`);
-    console.log(`Input City--> ${inputCity}`);
-    console.log(`Input State--> ${inputState}`);
-    console.log(`Input Country--> ${inputCountry}`);
-    console.log(`Input Shape--> ${inputShape}`);
-   
+  // Select the input element and get the raw HTML node
+  var inputDateTimeElement = d3.select("#datetime");
+  var inputCityElement = d3.select("#city");
+  var inputStateElement = d3.select("#state");
+  var inputCountryElement = d3.select("#country");
+  var inputShapeElement = d3.select("#shape");
 
-    // Series of If and Checks to see what data is being requested.
+  // Get the value property of the input elements
+  var inputDateTime = inputDateTimeElement.property("value");
+  var inputCity = inputCityElement.property("value");
+  var inputState = inputStateElement.property("value");
+  var inputCountry = inputCountryElement.property("value");
+  var inputShape = inputShapeElement.property("value");
 
-    if (inputDateTime === "") {
-      var filteredData = tableData.filter(ufoData => ufoData.datetime);
-    }
-    else {
-      var filteredData = tableData.filter(ufoData => ufoData.datetime === inputDateTime);
-    }
+  console.log(`Input Date--> ${inputDateTime}`);
+  console.log(`Input City--> ${inputCity}`);
+  console.log(`Input State--> ${inputState}`);
+  console.log(`Input Country--> ${inputCountry}`);
+  console.log(`Input Shape--> ${inputShape}`);
 
-    if (inputCity === "") {
-      var filteredData = filteredData.filter(ufoData => ufoData.city);
-    }
-    else {
-      var filteredData = filteredData.filter(ufoData => ufoData.city === inputCity);
-    }
 
-    if (inputState === "") {
-      var filteredData = filteredData.filter(ufoData => ufoData.state);
-    }
-    else {
-      var filteredData = filteredData.filter(ufoData => ufoData.state === inputState);
-    }
+  // Series of If and Checks to see what data is being requested.
 
-    if (inputCountry === "") {
-      var filteredData = filteredData.filter(ufoData => ufoData.country);
-    }
-    else {
-      var filteredData = filteredData.filter(ufoData => ufoData.country === inputCountry);
-    }
+  if (inputDateTime === "") {
+    var filteredData = tableData.filter(ufoData => ufoData.datetime);
+  }
+  else {
+    var filteredData = tableData.filter(ufoData => ufoData.datetime === inputDateTime);
+  }
 
-    if (inputShape === "") {
-      var filteredData = filteredData.filter(ufoData => ufoData.shape);
-    }
-    else {
-      var filteredData = filteredData.filter(ufoData => ufoData.shape === inputShape);
-    }
+  if (inputCity === "") {
+    var filteredData = filteredData.filter(ufoData => ufoData.city);
+  }
+  else {
+    var filteredData = filteredData.filter(ufoData => ufoData.city === inputCity);
+  }
 
-    console.log("Filtered Data Next");
-    console.log(filteredData);
+  if (inputState === "") {
+    var filteredData = filteredData.filter(ufoData => ufoData.state);
+  }
+  else {
+    var filteredData = filteredData.filter(ufoData => ufoData.state === inputState);
+  }
 
-    //Clean-up previous data in the html table if it is there.
-    tbody.html("")
+  if (inputCountry === "") {
+    var filteredData = filteredData.filter(ufoData => ufoData.country);
+  }
+  else {
+    var filteredData = filteredData.filter(ufoData => ufoData.country === inputCountry);
+  }
 
-    //load the HTML table to display data.
-    filteredData.forEach((UFOsighting) => {
-        var row = tbody.append("tr");
-        Object.entries(UFOsighting).forEach(([key, value]) => {
-          var cell = row.append("td");
-          cell.text(value);
-        });
+  if (inputShape === "") {
+    var filteredData = filteredData.filter(ufoData => ufoData.shape);
+  }
+  else {
+    var filteredData = filteredData.filter(ufoData => ufoData.shape === inputShape);
+  }
+
+  console.log("Filtered Data Next");
+  console.log(filteredData);
+
+  //Clean-up previous data in the html table if it is there.
+  tbody.html("");
+
+  //load the HTML table to display data.
+  filteredData.forEach((UFOsighting) => {
+    var row = tbody.append("tr");
+    Object.entries(UFOsighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
     });
-      
-};
+  });
+
+}
